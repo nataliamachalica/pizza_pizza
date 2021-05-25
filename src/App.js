86e84730d45ed84559061b3d3d446b5d1df1ded3
Natalia Szlapka <n.machalica@gmail.com>
2021-05-25 14:35:49 +0200

    Add firts UI

--- src/App.js
+++ src/App.js
@@ -9,22 +9,36 @@ import TablesBooking from '../src/components/views/TablesBooking/TablesBooking';
 import TablesEvents from '../src/components/views/TablesEvents/TablesEvents';
 import Waiter from '../src/components/views/Waiter/Waiter';
 import WaiterOrder from '../src/components/views/WaiterOrder/WaiterOrder';
+import { StylesProvider } from '@material-ui/core/styles';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: { main: '#2B4C6F' },
+    //secondary: { main: '#11cb5f' },
+  },
+});
 
 function App() {
   return (
     <BrowserRouter>
-      <MainLayout>
-        <Switch>
-          <Route exact path={`${process.env.PUBLIC_URL}/`} component={Homepage} />
-          <Route exact path={process.env.PUBLIC_URL + '/login'} component={Login} />
-          <Route exact path={process.env.PUBLIC_URL + '/kitchen'} component={Kitchen} />
-          <Route exact path={process.env.PUBLIC_URL + '/tables'} component={Tables} />
-          <Route exact path={process.env.PUBLIC_URL + '/tables/booking/:id'} component={TablesBooking} />
-          <Route exact path={process.env.PUBLIC_URL + '/tables/events/:id'} component={TablesEvents} />
-          <Route exact path={process.env.PUBLIC_URL + '/waiter'} component={Waiter} />
-          <Route exact path={process.env.PUBLIC_URL + '/waiter/order/:id'} component={WaiterOrder} />
-        </Switch>
-      </MainLayout>
+      <StylesProvider injectFirst>
+        <ThemeProvider theme={theme}>
+          <MainLayout>
+            <Switch>
+              <Route exact path={`${process.env.PUBLIC_URL}/`} component={Homepage} />
+              <Route exact path={process.env.PUBLIC_URL + '/login'} component={Login} />
+              <Route exact path={process.env.PUBLIC_URL + '/kitchen'} component={Kitchen} />
+              <Route exact path={process.env.PUBLIC_URL + '/tables'} component={Tables} />
+              <Route exact path={process.env.PUBLIC_URL + '/tables/booking/:id'} component={TablesBooking} />
+              <Route exact path={process.env.PUBLIC_URL + '/tables/events/:id'} component={TablesEvents} />
+              <Route exact path={process.env.PUBLIC_URL + '/waiter'} component={Waiter} />
+              <Route exact path={process.env.PUBLIC_URL + '/waiter/order/:id'} component={WaiterOrder} />
+            </Switch>
+          </MainLayout>
+        </ThemeProvider>
+      </StylesProvider>
     </BrowserRouter>
   );
 }
--- src/components/layout/MainLayout/MainLayout.js
+++ src/components/layout/MainLayout/MainLayout.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import PageNav from '../PageNav/PageNav';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Container from '@material-ui/core/Container';
 
-const MainLayout = ({children}) => {
-  return (
-    <div>
-      <PageNav />
+
+const MainLayout = ({children}) => (
+  <div className="MainLayout">
+    <AppBar>
+      <Container maxWidth='lg'>
+        <Toolbar disableGutters>
+          <PageNav />
+        </Toolbar>
+      </Container>
+    </AppBar>
+
+    <Container maxWidth='lg'>
+      <Toolbar />
       {children}
-    </div>
-  );
-};
+    </Container>
+  </div>
+);
+
 
 MainLayout.propTypes = {
   children: PropTypes.node,
